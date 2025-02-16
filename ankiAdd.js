c763eb05fce2d6efa5943995c53830fd3f885e9a
Bluskyo <blusky445@gmail.com>
2025-02-16 13:12:52 +0100

    Radded regex to check if selection is japanese.

--- ankiAdd.js
+++ ankiAdd.js
@@ -1,6 +1,11 @@
+const jpRe = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/;
+
 document.addEventListener("mouseup", () => {
     let selection = window.getSelection().toString().trim();
-    if (selection) {
+
+    let isJapanese = jpRe.test(selection);
+
+    if (selection.length < 20 && isJapanese){
         browser.runtime.sendMessage({ action: "sendText", text: selection }).then(() => {
         }).catch(error => console.error("Error sending message:", error));
     }
