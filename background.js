dd6594fa3a378c0f9aced407d6d6cb863c5ffdc4
Bluskyo <blusky445@gmail.com>
2025-02-17 12:56:58 +0100

    basic fetch and display of word data,

--- background.js
+++ background.js
@@ -1,10 +1,38 @@
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "sendText") {
         browser.storage.local.set({ selectedText: message.text }).then(() => {});
-    } 
-    else if (message.action === "getText") {
-        browser.storage.local.get("selectedText").then((result) => {
-            sendResponse({ text: result.selectedText || "" });
+
+        let selectedText = message.text
+
+        fetch("http://localhost:8080/api/v1/" + selectedText)
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error("Could not find word!")
+                }
+            })
+            .then(data => {
+                browser.storage.local.set({ 
+                    reading: data.reading, 
+                    meaning: data.gloss,
+                    pos: data.pos
+                 }).then(() => {});
+            })
+            .catch(error => console.log("Error! ", error))
+    
+
+    } else if (message.action === "getText") {
+        browser.storage.local.get([
+            "selectedText", 
+            "reading", 
+            "meaning", 
+            "pos"])
+            .then((result) => {
+                sendResponse({ text: result.selectedText, 
+                    re: result.reading, 
+                    meaning: result.meaning, 
+                    pos: result.pos|| "" });
         });
 
         return true; // keeps the response channel open for async func
--- popup/info.js
+++ popup/info.js
@@ -1,6 +1,9 @@
 browser.runtime.sendMessage({ action: "getText" }).then(response => {
     if (response && response.text) {
         document.getElementById("selected-text").textContent = response.text;
+        document.getElementById("reading").textContent = response.re;
+        document.getElementById("meaning").textContent = response.meaning;
+        document.getElementById("tag").textContent = response.pos;
     } else {
         document.getElementById("selected-text").textContent = "";
     }
