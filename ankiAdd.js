e7365e5e4b7e8d93ea38e235c0efe596f2e0e8fc
root <root@Fishas>
2025-04-02 19:11:29 +0200

    detects selected text in input fields

--- ankiAdd.js
+++ ankiAdd.js
@@ -1,11 +1,19 @@
 const japaneseRE = /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/;
 
 document.addEventListener("mouseup", () => {
-    let selection = window.getSelection().toString().trim();
+    let selection = document.getSelection().toString().trim();
+    const currentElement = document.activeElement;
+    const selectionStart = currentElement.selectionStart;
+    const selectionEnd = currentElement.selectionEnd;
 
-    let isJapanese = japaneseRE.test(selection);
+    if (selection == ""){ // checks input tag, getselection cant read these.
+        const input = currentElement.value;
+        selection = input.substring(selectionStart, selectionEnd);
+    }
 
-    if (isJapanese){
+    let containsJP = japaneseRE.test(selection);
+
+    if (containsJP){
         let currentLocation = window.location.href;
         browser.runtime.sendMessage({
             action: "saveSelection",
